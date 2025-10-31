8ffa853be6aba0489d833891506a51402a761962
Manuelchap <c.josechaperno19@gmail.com>
2025-10-31 12:04:28 -0600

    1er code

--- Ejercicio 2 c++.cpp
+++ Ejercicio 2 c++.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main(){
 	
 	for(int i = 0; i <= 100; i++){ 
+
 		
     if(i % 3 == 0 && i % 5 == 0){ 
         cout << i << " fizzbuzz" << endl;
@@ -20,3 +21,4 @@ int main(){
 
 
 }
+
